6b48daf7c65d40144b3a36475fe0e0ab0369eff7
Fredray-21 <dabadie.frederic@gmail.com>
2023-10-28 19:38:09 +0200

    add(test) : ajout test getAll/add/remove user

--- src/user/UserJSONService.test.ts
+++ src/user/UserJSONService.test.ts
@@ -29,22 +29,43 @@ describe('UserJSONService', () => {
     });
 
     it("doit renvoyer un utilisateur par son identifiant s'il existe", () => {
-        fsMock.readFileSync.mockReturnValue(
-            JSON.stringify(dataTest),
-        );
+        fsMock.readFileSync.mockReturnValue(JSON.stringify(dataTest));
 
         const user = userService.getById(2);
         expect(user).not.toBeNull();
         expect(user?.getId()).toBe(2);
-
     });
 
     it("devrait renvoyer null pour un identifiant qui n'existe pas", () => {
-        fsMock.readFileSync.mockReturnValue(
-            JSON.stringify(dataTest),
-        );
+        fsMock.readFileSync.mockReturnValue(JSON.stringify(dataTest));
 
         const user = userService.getById(12);
         expect(user).toBeNull();
+    });
+
+    it('doit renvoyer tous les utilisateurs', () => {
+        fsMock.readFileSync.mockReturnValue(JSON.stringify(dataTest));
+
+        const users = userService.getAll();
+        expect(users).toHaveLength(2);
+        expect(users[0].getId()).toBe(1);
+        expect(users[1].getId()).toBe(2);
+    });
+
+    it('doit ajouter un utilisateur', () => {
+        fsMock.existsSync.mockReturnValue(true); // Simuler l'existence du fichier
+        fsMock.readFileSync.mockReturnValue(JSON.stringify(dataTest));
+        fsMock.writeFileSync.mockImplementation(() => {}); // Éviter d'écrire réellement dans le fichier
+
+        const user = userService.add('user3', 'emailUser2@user.com', 'paSsw0rd-(');
+
+        expect(user).not.toBeNull();
+        expect(user?.getId()).toBe(3);
+    });
+
+    it('doit supprimer un utilisateur', () => {
+        fsMock.readFileSync.mockReturnValue(JSON.stringify(dataTest));
+        const isDeleted = userService.remove(2);
+        expect(isDeleted).toBeTruthy();
     });
 });
